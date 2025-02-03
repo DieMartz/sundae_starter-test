b27c1f22dd6c781c19d8860d33ae1b6c304a7344
Diego Sanchez <diewow.martz@gmail.com>
2025-02-03 16:08:44 -0600

    test if dependency is resolver from react-bootstrap

--- src/pages/orderEntry/OrderEntry.jsx
+++ src/pages/orderEntry/OrderEntry.jsx
@@ -2,21 +2,21 @@ import React from "react";
 import Options from "./Options";
 import { useOrderDetails } from "../../context/OderDetails";
 import { formatCurrency } from "../../utilities";
-import Button from "react-bootstrap/Button";
+import { Button } from "./node_modules/react-bootstrap";
 
-export default function OrderEntry( { setOrderPhase } ) {
+export default function OrderEntry({ setOrderPhase }) {
+  const { totals } = useOrderDetails();
 
-    const { totals } = useOrderDetails();
+  const isEnabled = totals.scoops === 0;
 
-    const isEnabled = totals.scoops === 0; 
-
-
-    return (
-        <div>
-            <Options optionType="scoops"/>
-            <Options optionType="toppings"/>
-            <h2>Grand total: {formatCurrency( totals.scoops + totals.toppings )} </h2>
-            <Button onClick={() => setOrderPhase( "review" )} disabled={isEnabled} >Order Sundae!</Button>
-        </div>
-    );
+  return (
+    <div>
+      <Options optionType="scoops" />
+      <Options optionType="toppings" />
+      <h2>Grand total: {formatCurrency(totals.scoops + totals.toppings)} </h2>
+      <Button onClick={() => setOrderPhase("review")} disabled={isEnabled}>
+        Order Sundae!
+      </Button>
+    </div>
+  );
 }
